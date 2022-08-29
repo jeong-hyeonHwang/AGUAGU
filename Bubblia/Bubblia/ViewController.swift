7b6bc143ec8220cf403a06ac9c619a6bffb0eff8
jeonghyeon Hwang <hijihilin@gmail.com>
2022-08-29 15:12:12 +0900

    [FEAT] Add Initial Project

---
+++ Bubblia/Bubblia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bubblia
+//
+//  Created by 황정현 on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
