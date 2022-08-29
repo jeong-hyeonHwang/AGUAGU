f1ade62a52923487e99eea4ce32091e9b52060d5
jeonghyeon Hwang <hijihilin@gmail.com>
2022-08-29 15:46:36 +0900

    [FEAT] Add Default Camera View

--- Bubblia/Bubblia/AppDelegate.swift
+++ Bubblia/Bubblia/AppDelegate.swift
@@ -32,6 +32,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    enum AppError: Error {
+        case captureSessionSetup(reason: String)
+        case visionError(error: Error)
+        case otherError(error: Error)
+        
+        static func display(_ error: Error, inViewController viewController: UIViewController) {
+            if let appError = error as? AppError {
+                appError.displayInViewController(viewController)
+            } else {
+                AppError.otherError(error: error).displayInViewController(viewController)
+            }
+        }
+        
+        func displayInViewController(_ viewController: UIViewController) {
+            let title: String?
+            let message: String?
+            switch self {
+            case .captureSessionSetup(let reason):
+                title = "AVSession Setup Error"
+                message = reason
+            case .visionError(let error):
+                title = "Vision Error"
+                message = error.localizedDescription
+            case .otherError(let error):
+                title = "Error"
+                message = error.localizedDescription
+            }
+            
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            viewController.present(alert, animated: true, completion: nil)
+        }
+    }
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
@@ -76,6 +110,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
+
+enum AppError: Error {
+    case captureSessionSetup(reason: String)
+    case visionError(error: Error)
+    case otherError(error: Error)
+    
+    static func display(_ error: Error, inViewController viewController: UIViewController) {
+        if let appError = error as? AppError {
+            appError.displayInViewController(viewController)
+        } else {
+            AppError.otherError(error: error).displayInViewController(viewController)
+        }
+    }
+    
+    func displayInViewController(_ viewController: UIViewController) {
+        let title: String?
+        let message: String?
+        switch self {
+        case .captureSessionSetup(let reason):
+            title = "AVSession Setup Error"
+            message = reason
+        case .visionError(let error):
+            title = "Vision Error"
+            message = error.localizedDescription
+        case .otherError(let error):
+            title = "Error"
+            message = error.localizedDescription
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
+
 
--- Bubblia/Bubblia/ViewController.swift
+++ Bubblia/Bubblia/ViewController.swift
@@ -6,14 +6,79 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    private var cameraView: CameraView { view as! CameraView }
+    
+    private let videoDataOutputQueue = DispatchQueue(label: "CameraFeedDataOutput", qos: .userInteractive)
+    private var cameraFeedSession: AVCaptureSession?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        do {
+            if cameraFeedSession == nil {
+                cameraView.previewLayer.videoGravity = .resizeAspectFill
+                try setupAVSession()
+                cameraView.previewLayer.session = cameraFeedSession
+            }
+            cameraFeedSession?.startRunning()
+        } catch {
+            AppError.display(error, inViewController: self)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        cameraFeedSession?.stopRunning()
+        super.viewWillDisappear(animated)
+    }
+    
+    func setupAVSession() throws {
+        // Select a front facing camera, make an input.
+        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front) else {
+            throw AppError.captureSessionSetup(reason: "Could not find a front facing camera.")
+        }
+        
+        guard let deviceInput = try? AVCaptureDeviceInput(device: videoDevice) else {
+            throw AppError.captureSessionSetup(reason: "Could not create video device input.")
+        }
+        
+        let session = AVCaptureSession()
+        session.beginConfiguration()
+        session.sessionPreset = AVCaptureSession.Preset.high
+        
+        // Add a video input.
+        guard session.canAddInput(deviceInput) else {
+            throw AppError.captureSessionSetup(reason: "Could not add video device input to the session")
+        }
+        session.addInput(deviceInput)
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        if session.canAddOutput(dataOutput) {
+            session.addOutput(dataOutput)
+            // Add a video data output.
+            dataOutput.alwaysDiscardsLateVideoFrames = true
+            dataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+            dataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        } else {
+            throw AppError.captureSessionSetup(reason: "Could not add video data output to the session")
+        }
+        session.commitConfiguration()
+        cameraFeedSession = session
+    }
+    
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+    }
+    
+}
