b6079289a90385fea8e8f2d6827cb3c13a161180
jeonghyeon Hwang <hijihilin@gmail.com>
2022-08-31 04:02:07 +0900

    [DESIGN] Change Player Shape

--- Bubblia/Bubblia/CameraView.swift
+++ Bubblia/Bubblia/CameraView.swift
@@ -53,9 +53,45 @@ class CameraView: UIView {
     // MARK: SHOW POINTS
     func showPoints(_ points: [CGPoint], color: UIColor) {
         pointsPath.removeAllPoints()
-        for point in points {
-            pointsPath.move(to: point)
-            pointsPath.addArc(withCenter: point, radius: 5, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+        for idx in 0..<points.count {
+            let point = points[idx]
+            pointsPath.move(to: CGPoint(x: point.x, y: point.y))
+//            pointsPath.addLine(to: CGPoint(x: point.x+10, y: point.y))
+//            pointsPath.addArc(withCenter: point, radius: 10, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+            
+            if (idx+1) % 2 == 0 {
+                pointsPath.move(to: CGPoint(x: point.x-40, y: point.y))
+                let leftEyeCenter = CGPoint(x: point.x-20, y: point.y - 20)
+                let rightEyeCenter = CGPoint(x: point.x+20, y: point.y - 20)
+                            pointsPath.addArc(withCenter: leftEyeCenter, radius: 8, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+                pointsPath.move(to: rightEyeCenter)
+                            pointsPath.addArc(withCenter: rightEyeCenter, radius: 8, startAngle: 0, endAngle: 2 * .pi, clockwise: true)
+                pointsPath.move(to: CGPoint(x: point.x-40, y: point.y))
+//                pointsPath.addLine(to: CGPoint(x: point.x-40, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x-30, y: point.y + 10))
+                pointsPath.addLine(to: CGPoint(x: point.x-20, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x-10, y: point.y + 10))
+                pointsPath.addLine(to: CGPoint(x: point.x, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x+10, y: point.y + 10))
+                pointsPath.addLine(to: CGPoint(x: point.x+20, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x+30, y: point.y + 10))
+                pointsPath.addLine(to: CGPoint(x: point.x+40, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x+40, y: point.y-5))
+                pointsPath.addLine(to: CGPoint(x: point.x-40, y: point.y-5))
+            } else {
+                pointsPath.move(to: CGPoint(x: point.x-40, y: point.y+5))
+                pointsPath.addLine(to: CGPoint(x: point.x-40, y: point.y - 10))
+                pointsPath.addLine(to: CGPoint(x: point.x-30, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x-20, y: point.y - 10))
+                pointsPath.addLine(to: CGPoint(x: point.x-10, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x, y: point.y - 10))
+                pointsPath.addLine(to: CGPoint(x: point.x+10, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x+20, y: point.y - 10))
+                pointsPath.addLine(to: CGPoint(x: point.x+30, y: point.y))
+                pointsPath.addLine(to: CGPoint(x: point.x+40, y: point.y - 10))
+                pointsPath.addLine(to: CGPoint(x: point.x+40, y: point.y+5))
+                pointsPath.addLine(to: CGPoint(x: point.x-40, y: point.y+5))
+            }
         }
         overlayLayer.fillColor = color.cgColor
         CATransaction.begin()
