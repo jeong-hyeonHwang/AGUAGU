3ad4f66e45a02a05070a1ff8d52138547ad1571b
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-09 00:01:02 +0900

    [FEAT] Add CGPoint Distance Calc Method

--- Bubblia/Bubblia/Extensions/CGPoint+.swift
+++ Bubblia/Bubblia/Extensions/CGPoint+.swift
@@ -16,4 +16,13 @@ extension CGPoint {
     func distance(from point: CGPoint) -> CGFloat {
         return hypot(point.x - x, point.y - y)
     }
+
+}
+
+func CGPointDistanceSquared(from: CGPoint, to: CGPoint) -> CGFloat {
+    return (from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)
+}
+
+func CGPointDistance(from: CGPoint, to: CGPoint) -> CGFloat {
+    return sqrt(CGPointDistanceSquared(from: from, to: to))
 }
