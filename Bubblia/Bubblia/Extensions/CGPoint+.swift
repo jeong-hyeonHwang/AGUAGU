8b7a6174f7100e602eeeec8da3656d4546ac41da
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-08 22:21:45 +0900

    [FEAT] Add Extension Folder

---
+++ Bubblia/Bubblia/Extensions/CGPoint+.swift
@@ -0,0 +1,19 @@
+//
+//  CGPoint+.swift
+//  Bubblia
+//
+//  Created by 황정현 on 2022/09/08.
+//
+
+import UIKit
+
+extension CGPoint {
+
+    static func midPoint(p1: CGPoint, p2: CGPoint) -> CGPoint {
+        return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
+    }
+    
+    func distance(from point: CGPoint) -> CGFloat {
+        return hypot(point.x - x, point.y - y)
+    }
+}
--- Bubblia/Bubblia/HandGestureProcessor.swift
+++ Bubblia/Bubblia/HandGestureProcessor.swift
@@ -62,14 +62,3 @@ class HandGestureProcessor {
 }
 
 // MARK: - CGPoint helpers
-
-extension CGPoint {
-
-    static func midPoint(p1: CGPoint, p2: CGPoint) -> CGPoint {
-        return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
-    }
-    
-    func distance(from point: CGPoint) -> CGFloat {
-        return hypot(point.x - x, point.y - y)
-    }
-}
