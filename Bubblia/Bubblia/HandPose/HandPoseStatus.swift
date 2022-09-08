9d0c5770a0e385ef39bdbb91a9ce4752ef32ff61
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-09 00:03:21 +0900

    [FEAT] Add HandPose Status Method

---
+++ Bubblia/Bubblia/HandPose/HandPoseStatus.swift
@@ -0,0 +1,14 @@
+//
+//  HandPoseStatus.swift
+//  Bubblia
+//
+//  Created by 황정현 on 2022/09/08.
+//
+
+import Foundation
+
+enum HandStatus: Int, CaseIterable {
+    case possible
+    case pinched
+    case invalid
+}
