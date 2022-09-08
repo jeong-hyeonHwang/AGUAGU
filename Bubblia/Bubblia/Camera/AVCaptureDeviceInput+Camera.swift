8e4d99265bda44900cbea683cf8b9c6e10af2219
jeonghyeon Hwang <hijihilin@gmail.com>
2022-09-08 23:10:52 +0900

    [FEAT] Add CameraInput Create Method

---
+++ Bubblia/Bubblia/Camera/AVCaptureDeviceInput+Camera.swift
@@ -0,0 +1,28 @@
+//
+//  AVCaptureDeviceInput+Camera.swift
+//  Bubblia
+//
+//  Created by 황정현 on 2022/09/08.
+//
+
+import AVFoundation
+
+extension AVCaptureDeviceInput {
+    static func createCameraInput(frameRate: Double) -> AVCaptureDeviceInput? {
+        guard let camera = AVCaptureDevice.default(.builtInWideAngleCamera,
+                                                   for: AVMediaType.video,
+                                                   position: .front) else {
+            return nil
+        }
+        
+        do {
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+
+            return cameraInput
+        } catch {
+            print("Unable to create an input from the camera: \(error)")
+            return nil
+        }
+    }
+}
+
